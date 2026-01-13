7f4ce3928325fd9c41742102720ad2ac7d453791
momentumnn <114093710+momentumnn@users.noreply.github.com>
2026-01-14 00:43:48 +0800

    Completed 1.1

---
+++ src/main/java/Circle.java
@@ -0,0 +1,50 @@
+public class Circle {
+    private int x;
+    private int y;
+    private double radius;
+    private static double maxRadius = 0;
+
+    public Circle(){
+        this(0,0,0);
+    }
+    public Circle(int x, int y, double radius){
+        setX(x);
+        setY(y);
+        setRadius(radius);
+        if(maxRadius < radius){
+            maxRadius = radius;
+        }
+    }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+    public void setRadius(double radius){
+        this.radius = Math.max(radius,0);
+        setMaxRadius(radius);
+    }
+
+    public static void setMaxRadius(double maxRadius) {
+        Circle.maxRadius = Math.max(maxRadius, getMaxRadius());
+    }
+
+    public int getX(){
+        return x;
+    }
+    public int getY(){
+        return y;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public static double getMaxRadius() {
+        return maxRadius;
+    }
+
+    public int getArea(){
+        return (int)(Math.PI * Math.pow(radius,2));
+    }
+}
+
